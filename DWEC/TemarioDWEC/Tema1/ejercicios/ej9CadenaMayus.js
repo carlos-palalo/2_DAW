1e98a3fb0f67dcd29980496efe16241bc47eb4c1
carlos-palalo <palaxcarlos@gmail.com>
2020-09-28 20:02:00 +0200

    actu

--- DWEC/TemarioDWEC/Tema1/ejercicios/ej9CadenaMayus.js
+++ DWEC/TemarioDWEC/Tema1/ejercicios/ej9CadenaMayus.js
@@ -2,12 +2,10 @@ var texto = "En la clase de informática tenemos ordenadores.";
 var tFinal = "";
 var lista = texto.split(" ");
 
-for (var i = 0; i < lista.length; i += 2) {
-    lista[i] = lista[i].toUpperCase();
-}
-
 for (var i = 0; i < lista.length; i++) {
+    if (i % 2 == 0) {
+        lista[i] = lista[i].toUpperCase();
+    }
     tFinal += lista[i] + " ";
 }
-
 console.log(tFinal);
