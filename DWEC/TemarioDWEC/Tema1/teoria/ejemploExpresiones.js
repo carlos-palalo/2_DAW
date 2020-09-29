aebc2e22a3b1b6091eceec908d1f4133680e98a9
carlos-palalo <palaxcarlos@gmail.com>
2020-09-29 18:38:59 +0200

    fgh

---
+++ DWEC/TemarioDWEC/Tema1/teoria/ejemploExpresiones.js
@@ -0,0 +1,6 @@
+console.log("/la*/");
+var expresion = /la*/;
+console.log(expresion.test("Hola"));
+console.log(expresion.test("Perro"));
+console.log(expresion.test("Pelé"));
+console.log(expresion.test("lloro"));
