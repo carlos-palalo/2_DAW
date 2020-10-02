f2c016cba8cacef9d23df23f53c67fbb6113c0e6
carlos-palalo <palaxcarlos@gmail.com>
2020-10-02 18:40:36 +0200

    asdfasdf

---
+++ DWES/Php/ejercicios/ej15/formulario15.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="estilos.css" rel="stylesheet" type="text/css">
+    <title>Ej15_PHP</title>
+</head>
+
+<body>
+    <h1>Ejercicio 15 PHP</h1>
+    <form name="Ejercicio 11" action="visor15.php" method="POST">
+        <p>Introduce un número: <input type="float" name="num" required></p>
+        <p> <textarea name="texto" rows="4" cols="50"></textarea></p>
+
+        <input type="submit" value="Enviar">
+        <input type="reset" value="Borrar">
+    </form>
+</body>
+
+</html>
