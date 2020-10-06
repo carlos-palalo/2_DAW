f5d20d939c14f4703c67f9659f6f20a24336df4f
carlos-palalo <palaxcarlos@gmail.com>
2020-10-06 19:47:37 +0200

    asdfsdf

--- DWEC/TemarioDWEC/Tema1/ejercicios/ej18uuid.js
+++ DWEC/TemarioDWEC/Tema1/ejercicios/ej18uuid.js
@@ -2,8 +2,11 @@
 function random(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
+
 var cantidad = 100;
+var vector = Array(cantidad);
 var uuid;
+
 for (let j = 0; j < cantidad; j++) {
     uuid = "";
     uuid += "{";
@@ -13,6 +16,21 @@ for (let j = 0; j < cantidad; j++) {
         }
         uuid += random(0, 15).toString(16);
     }
-    uuid +="}";
-    console.log(uuid);
+    uuid += "}";
+
+    if (j == 0)
+        vector[j] = uuid.toUpperCase();
+    else {
+        for (let z = 0; z < j; z++) {
+            if (vector[z] == uuid) {
+                console.log("Error. UUID Repetido!");
+                return;
+            } else {
+                vector[j] = uuid.toUpperCase();
+            }
+        }
+    }
 }
+
+console.log("-------------- UUID Generados -------------")
+console.log(vector);
