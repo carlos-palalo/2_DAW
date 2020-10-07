fc3e1d5bc5f0b4c47cafa116256d776eeff5ca16
carlos-palalo <palaxcarlos@gmail.com>
2020-10-07 19:38:27 +0200

    asfasdf

---
+++ DWEC/TemarioDWEC/Tema2/ejercicios/ej2Coche.js
@@ -0,0 +1,53 @@
+const distanciaVuelta = 7000;
+const vueltasTotales = 9;
+
+function Coche(nombre, potencia, combustible) {
+    this.nombre = nombre;
+    this.potencia = potencia;
+    this.combustible = combustible;
+    this.tiempo = 0;
+    this.vueltas = 0;
+    this.recorrido = 0;
+
+    this.acelerar = function () {
+        if (this.combustible > 0) {
+            var acelerado = Math.floor(Math.random() * this.potencia);
+            this.recorrido += acelerado;
+            this.tiempo++;
+            this.combustible = this.combustible - acelerado * 0.000999;
+        }
+    }
+}
+
+var carrera = [];
+
+carrera.push(new Coche("c1", 110, 60));
+carrera.push(new Coche("c2", 100, 60));
+carrera.push(new Coche("c3", 120, 60));
+carrera.push(new Coche("c4", 130, 60));
+carrera.push(new Coche("c5", 115, 60));
+carrera.push(new Coche("c6", 110, 60));
+
+while (carrera.every(y => y.vueltas < vueltasTotales)) {
+    if (carrera.every(x => x.combustible <= 0)) {
+        console.log("TODOS SIN GASOLINA");
+        break;
+    }
+
+    carrera.forEach(x => { x.acelerar(); });
+
+    if (carrera.some(x => {
+        if (x.recorrido >= distanciaVuelta) {
+            x.vueltas++;
+            x.recorrido -= distanciaVuelta;
+        }
+    }));
+}
+
+//RESULTADOS DE LA CARRERA
+carrera.forEach(x => console.log(x.nombre + " --> " + x.vueltas + " --> " + x.recorrido + " --> " + x.combustible + " --> " + x.tiempo));
+carrera.forEach(x => {
+    if (x.vueltas == vueltasTotales) {
+        console.log("GANADOR: " + x.nombre);
+    }
+})
