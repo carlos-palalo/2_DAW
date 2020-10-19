06ba677cea4ac08925983c7facc696dbc68ad36f
carlos-palalo <palaxcarlos@gmail.com>
2020-10-19 22:07:55 +0200

    sdfasdfa

--- DWEC/TemarioDWEC/Tema3/ejercicios/ej3.js
+++ DWEC/TemarioDWEC/Tema3/ejercicios/ej3.js
@@ -1,22 +1,23 @@
 //Cambiar el año de una fecha (++ o --) dependiendo si un dia es par o impar
 
 var personas = '[\
-    {"nombre":"pepe","fechanac":"1980-04-06"}\
+    {"nombre":"pepe","fechanac":"1980-05-06"},\
+    {"nombre":"julian","fechanac":"1982-05-02"},\
+    {"nombre":"carlos","fechanac":"1986-02-03"},\
+    {"nombre":"jose","fechanac":"1930-03-20"},\
+    {"nombre":"ana","fechanac":"1995-05-21"},\
+    {"nombre":"maria","fechanac":"1957-04-01"}\
 ]';
 var dia = new Date();
 
 var obj = JSON.parse(personas, function (key, value) {
     if (key == "fechanac") {
-        fecha = new Date(value);
-        dia = fecha.getDate();
-        console.log(dia);
-        if (dia % 2 == 0) {
-            fecha.getFullYear()++;
-        } else {
-            fecha.getFullYear()--;
-        }
-        
-        return fecha;
+        dia = new Date(value);
+        if (dia.getDate() % 2 == 0)
+            dia.setFullYear(dia.getFullYear() + 1);
+        else
+            dia.setFullYear(dia.getFullYear() - 1);
+        return dia;
     }
     return value;
 });
