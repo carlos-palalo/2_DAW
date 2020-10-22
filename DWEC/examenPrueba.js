f01666d02130cb245a7d9f767848e1ce2a3c2d1e
carlos-palalo <palaxcarlos@gmail.com>
2020-10-22 19:52:59 +0200

    adfasdf

--- DWEC/examenPrueba.js
+++ DWEC/examenPrueba.js
@@ -4,7 +4,7 @@ var cad1 = {
         { "codigo": 1, "enunciado": "La suma de dos y dos son...", "respuesta1": "3", "respuesta2": "4", "respuesta3": "5", "correcta": "2" },
         { "codigo": 2, "enunciado": "El rio Pisuerga pasa por...", "respuesta1": "Murcia", "respuesta2": "Almería", "respuesta3": "Valladolid", "correcta": "3" },
         { "codigo": 4, "enunciado": "El número de módulos en segundo de DAW es...", "respuesta1": "4", "respuesta2": "5", "respuesta3": "6", "correcta": "2" },
-        { "codigo": 3, "enunciado": "El día de Navidad es el 25 de...", "respuesta1": "Enero", "respuesta2": "Noviembre", "respuesta3": "Diciembre", "correcta": "3" },
+        { "codigo": 3, "enunciado": "El día de Navidad es el 25 de...", "respuesta1": "Enero", "respuesta2": "Noviembre", "respuesta3": "Diciembre", "correcta": "3" }
     ]
 };
 
