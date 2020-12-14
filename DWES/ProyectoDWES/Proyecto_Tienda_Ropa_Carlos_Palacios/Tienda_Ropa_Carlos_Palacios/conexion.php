b50ac720c15297f4d052c9c7dc4e6ca1100c1f59
carlos-palalo <carlos-palalo@gmail.com>
2020-12-14 16:07:57 +0100

    asdasdf

--- DWES/ProyectoDWES/Proyecto_Tienda_Ropa_Carlos_Palacios/Tienda_Ropa_Carlos_Palacios/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-$servidor = 'localhost';
-$db = 'proyectotiendaropa_carlosp';
-$usuario = 'carlos';
-$contrasenia = 'carlos';
-$bd = new PDO('mysql:host=' . $servidor . ';dbname=' . $db, $usuario, $contrasenia);
