f686610bf9e7f3c92f6290029c7e6ad01471f307
carlos-palalo <palaxcarlos@gmail.com>
2021-02-01 21:41:31 +0100

    adsfadf

--- DINT/practicaMusic/js/main.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
