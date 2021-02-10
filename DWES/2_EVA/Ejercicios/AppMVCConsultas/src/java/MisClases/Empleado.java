2acfbdac6f78064b6063689ce8860241bcc509ab
carlos-palalo <carlos-palalo@gmail.com>
2021-02-10 21:10:49 +0100

    asdsdf

---
+++ DWES/2_EVA/Ejercicios/AppMVCConsultas/src/java/MisClases/Empleado.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MisClases;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author daw
+ */
+public class Empleado {
+
+    private int empno;
+    private String apellido;
+    private String oficio;
+    private int dir;
+    private java.sql.Date fechaalt;
+    private float salario;
+    private float comision;
+    private byte deptno;
+
+    public Empleado() {
+    }
+
+    public Empleado(int empno, String apellido, String oficio, int dir, java.sql.Date fechaalt, float salario, float comision, byte deptno) {
+        this.empno = empno;
+        this.apellido = apellido;
+        this.oficio = oficio;
+        this.dir = dir;
+        this.fechaalt = fechaalt;
+        this.salario = salario;
+        this.comision = comision;
+        this.deptno = deptno;
+    }
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getOficio() {
+        return oficio;
+    }
+
+    public void setOficio(String oficio) {
+        this.oficio = oficio;
+    }
+
+    public int getDir() {
+        return dir;
+    }
+
+    public void setDir(int dir) {
+        this.dir = dir;
+    }
+
+    public java.sql.Date getFechaalt() {
+        return fechaalt;
+    }
+
+    public void setFechaalt(java.sql.Date fechaalt) {
+        this.fechaalt = fechaalt;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public float getComision() {
+        return comision;
+    }
+
+    public void setComision(float comision) {
+        this.comision = comision;
+    }
+
+    public byte getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(byte deptno) {
+        this.deptno = deptno;
+    }
+}   //fin clase Empleado
---
+++ DWES/2_EVA/Ejercicios/AppMVCConsultas/src/java/MisClases/OperacionesBD.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MisClases;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author daw
+ */
+public class OperacionesBD {
+
+    public Connection getConnection() {
+        Connection conexion = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance(); //Cargar el driver
+            conexion = java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3306/carlos", "carlos", "carlos");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conexion;
+    }
+
+    //LISTAR - Devuelve un array con la lista de empleados
+    public ArrayList listarEmp() {
+        ArrayList empleados = new ArrayList();
+        try {
+            Connection conexion = getConnection();
+            Statement sentencia = conexion.createStatement();
+            String sql = "SELECT * FROM empleados";
+            ResultSet resul = sentencia.executeQuery(sql);
+            while (resul.next()) {  //Se crea un array con los datos de los empleados
+                //Empleado e = new Empleado(resul.getInt("dept_no"), resul.getString("dnombre"), resul.getString("loc"));
+                Empleado e = new Empleado(resul.getInt("emp_no"), resul.getString("apellido"), resul.getString("oficio"), resul.getInt("dir"), resul.getDate("fecha_alt"), resul.getFloat("salario"), resul.getFloat("comision"), resul.getByte("dept_no"));
+
+                empleados.add(e); //Añadir dep al array
+            }
+            conexion.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return (empleados);
+    } //fin listarDep
+
+    //INSERTAR - Recibe los datos del departamento a insertar en la tabla
+    public void insertaEmpleado(Empleado e) {
+        try {
+            Connection conexion = getConnection();
+            Statement sentencia = conexion.createStatement();
+            String sql = "INSERT INTO empleados VALUES(" + e.getEmpno() + ",'" + e.getApellido() + "','" + e.getOficio() + "'," + e.getDir() + ",'" + fecha() + "'," + e.getSalario() + "," + e.getComision() + "," + e.getDeptno() + ")";
+            if (e.getDeptno() != 0) {
+                sentencia.execute(sql);
+            }
+            System.out.println("SQL: " + sql);
+            conexion.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    } //fin insertaDepartamento
+    
+    private java.sql.Date fecha(){
+        return new java.sql.Date(new java.util.Date().getTime()); 
+    }
+} //fin clase OperacionesBD
